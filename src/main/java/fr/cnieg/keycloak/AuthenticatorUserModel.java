789359fe4f6feefad215b4f4f7f5217cb3d2120a
luojia <luojia@hyron.co.jp>
2023-03-17 17:45:51 +0900

    adds support multi attributes for login

--- src/main/java/fr/cnieg/keycloak/AuthenticatorUserModel.java
+++ src/main/java/fr/cnieg/keycloak/AuthenticatorUserModel.java
@@ -18,16 +18,39 @@ public class AuthenticatorUserModel {
 
         if (authenticatorConfigModel != null && authenticatorConfigModel.getConfig() != null && authenticatorConfigModel.getConfig().get(
                 attributeKey2) != null && authenticatorConfigModel.getConfig().get(attributeRegex2) != null) {
+            ///support multi attributes used for login
+            ///solution 1
+            ///support user to set attribute as String with format like `attribute1;attribute2`
             String attributeKey = authenticatorConfigModel.getConfig().get(attributeKey2);
             String attributeRegex = authenticatorConfigModel.getConfig().get(attributeRegex2);
             if (userName.matches(attributeRegex)) {
-                List<UserModel> result = context.getSession().users()
-                        .searchForUserByUserAttributeStream(context.getRealm(), attributeKey, userName)
+                String[] attributeKeyList = attributeKey.split("##");
+                for (int i = 0; i < attributeKeyList.length; i++) {
+                    List<UserModel> result = context.getSession().users()
+                        .searchForUserByUserAttributeStream(context.getRealm(), attributeKeyList[i], userName)
                         .collect(Collectors.toList());
-                if (result.size() == 1) {
-                    return result.get(0);
+                    if (result.size() == 1) {
+                        return result.get(0);
+                    }
                 }
             }
+            ///solution 2
+            ///set the setting of the attribute type with `ProviderConfigProperty.MULTIVALUED_STRING_TYPE`
+            ///TODO how to get the attribute as list
+            // List<String> attributeKeysString = authenticatorConfigModel.getConfig().get(attributeKey2);
+            // System.console().printf(attributeKeysString);
+            // List<String> attributeKeys = attributeKeysString.toList();
+            // String attributeRegex = authenticatorConfigModel.getConfig().get(attributeRegex2);
+            // if (userName.matches(attributeRegex)) {
+            //     for (String attributeKey : attributeKeys) {
+            //         List<UserModel> result = context.getSession().users()
+            //             .searchForUserByUserAttributeStream(context.getRealm(), attributeKey, userName)
+            //             .collect(Collectors.toList());
+            //         if (result.size() == 1) {
+            //             return result.get(0);
+            //         }
+            //     }
+            // }
         }
         return null;
     }
